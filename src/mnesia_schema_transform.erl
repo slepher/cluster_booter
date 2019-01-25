d59420acd74c42081f2e6524d2c3753935c49dee
slepher <cxyfree@gmail.com>
2019-01-25 18:30:39 +0800

    fix bugs

--- src/mnesia_schema_transform.erl
+++ src/mnesia_schema_transform.erl
@@ -10,19 +10,22 @@
 
 %% API
 -export([parse_transform/2, format_error/1]).
+-export([mnesia_schema/2]).
+
+-include_lib("astranaut/include/quote.hrl").
 
 %%%===================================================================
 %%% API
 %%%===================================================================
 parse_transform(Forms, _Options) ->
-    ast_traverse:map(fun walk/2, Forms).
+    Opts = [{attrs, []}, {alias, mnesia_schema}, group_args, debug],
+    astranaut_macro:transform_macro(?MODULE, mnesia_schema, 2, Opts, Forms).
 
 format_error(Message) ->
     case io_lib:deep_char_list(Message) of
         true -> Message;
         _    -> io_lib:write(Message)
     end.
-
 
 %%--------------------------------------------------------------------
 %% @doc
@@ -33,17 +36,14 @@ format_error(Message) ->
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
-walk(pre, {call, Line, {atom, _Line, mnesia_schema}, Tables}) ->
+mnesia_schema(Tables, #{line := Line}) ->
     NNode = lists:map(fun update_table/1, Tables),
-    NNNode = list_node(NNode, Line),
-    NNNode;
-walk(_Type, Node) ->
-    Node.
+    quote(tuple_to_list({unquote_splicing(NNode)}), Line).
 
-list_node([Node], Line) ->
-    {cons, Line, Node, {nil, Line}};
-list_node([H|T], Line) ->
-    {cons, Line, H, list_node(T, Line)}.
+%list_node([Node], Line) ->
+%    {cons, Line, Node, {nil, Line}};
+%list_node([H|T], Line) ->
+%    {cons, Line, H, list_node(T, Line)}.
 
 update_table({atom, Line, Name}) ->
     atom_to_map(Name, Line);
